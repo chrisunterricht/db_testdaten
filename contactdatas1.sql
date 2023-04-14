fd930e5e191f566d3190b274a857d240a7e0a1d3
Chris <129590048+chrisunterricht@users.noreply.github.com>
2023-04-14 10:40:41 +0200

    Create contactdatas1.sql

---
+++ contactdatas1.sql
@@ -0,0 +1,14 @@
+INSERT INTO Contactdatas (strasse, hausnr, plz, ort, email, telefon_arbeit, mobil_privat)
+VALUES 
+  ('Musterstraße', '1', '12345', 'Berlin', '[email protected]', '+49 123 456 7890', '+49 123 456 7891'),
+  ('Musterstraße', '2', '12345', 'Berlin', '[email protected]', '+49 123 456 7892', '+49 123 456 7893'),
+  ('Beispielstraße', '3', '54321', 'Hamburg', '[email protected]', '+49 123 456 7894', '+49 123 456 7895'),
+  ('Beispielstraße', '4', '54321', 'Hamburg', '[email protected]', '+49 123 456 7896', '+49 123 456 7897'),
+  ('Teststraße', '5', '11111', 'München', '[email protected]', '+49 123 456 7898', '+49 123 456 7899'),
+  ('Teststraße', '6', '11111', 'München', '[email protected]', '+49 123 456 7900', '+49 123 456 7901'),
+  ('Probeallee', '7', '22222', 'Köln', '[email protected]', '+49 123 456 7902', '+49 123 456 7903'),
+  ('Probeallee', '8', '22222', 'Köln', '[email protected]', '+49 123 456 7904', '+49 123 456 7905'),
+  ('Modellstraße', '9', '33333', 'Frankfurt', '[email protected]', '+49 123 456 7906', '+49 123 456 7907'),
+  ('Modellstraße', '10', '33333', 'Frankfurt', '[email protected]', '+49 123 456 7908', '+49 123 456 7909'),
+  ('Demonstrationsstraße', '11', '44444', 'Düsseldorf', '[email protected]', '+49 123 456 7910', '+49 123 456 7911'),
+  ('Demonstrationsstraße', '12', '44444', 'Düsseldorf', '[email protected]', '+49 123 456 7912', '+49 123 456 7913');
