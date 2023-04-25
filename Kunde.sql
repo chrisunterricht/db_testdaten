d1a448d02b68f6da64834124f323490d7b305956
Chris <129590048+chrisunterricht@users.noreply.github.com>
2023-04-25 10:34:35 +0200

    Update Kunde.sql

--- Kunde.sql
+++ Kunde.sql
@@ -1,25 +1,61 @@
-CREATE TABLE Kunde
+CREATE TABLE Kunde 
 (
     Vorname	VARCHAR(512),
-    Nachname	VARCHAR(512),
+    Nachname VARCHAR(512),
     Strasse	VARCHAR(512),
-    HNr	VARCHAR(512),
+    HNr	VARCHAR(5),
     PLZ	CHAR(5),
-    Ort	VARCHAR(512),
+    Stadt	VARCHAR(512),
     Email	VARCHAR(512)
 );
 
-INSERT INTO Kunde (Elmar, Sander, Elfi-Altmann-Straße, 8, 23340, Stendal, Elmar.Sander@yahoo.com) VALUES ('Dörte', 'Steiner', '"Hans-Gerd-Rausch-Straße', '98b', '87899', 'Ulm', 'Dörte.Steiner@yahoo.com');
-INSERT INTO Kunde (Elmar, Sander, Elfi-Altmann-Straße, 8, 23340, Stendal, Elmar.Sander@yahoo.com) VALUES ('Edmund', 'Riedl', 'Appelplatz', '2/3', '70435', 'Fröndenberg/Ruhr', 'Edmund.Riedl@yahoo.com');
-INSERT INTO Kunde (Elmar, Sander, Elfi-Altmann-Straße, 8, 23340, Stendal, Elmar.Sander@yahoo.com) VALUES ('Mona', 'Fricke', 'Carina-Baumann-Gasse', '32', '36915', 'Bad Homburg vor der Höhe', 'Mona.Fricke@yahoo.com');
-INSERT INTO Kunde (Elmar, Sander, Elfi-Altmann-Straße, 8, 23340, Stendal, Elmar.Sander@yahoo.com) VALUES ('Janine', 'Feldmann', 'Weiseweg', '4', '20811', 'Lemgo', 'Janine.Feldmann@yahoo.com');
-INSERT INTO Kunde (Elmar, Sander, Elfi-Altmann-Straße, 8, 23340, Stendal, Elmar.Sander@yahoo.com) VALUES ('Hans-Josef', 'Lutz', 'Rennerplatz', '9/6', '19845', 'Sundern (Sauerland)', 'Hans-Josef.Lutz@yahoo.com');
-INSERT INTO Kunde (Elmar, Sander, Elfi-Altmann-Straße, 8, 23340, Stendal, Elmar.Sander@yahoo.com) VALUES ('Margot', 'Giese', 'Irmhild-Langer-Allee', '4c', '36412', 'Heidelberg', 'Margot.Giese@yahoo.com');
-INSERT INTO Kunde (Elmar, Sander, Elfi-Altmann-Straße, 8, 23340, Stendal, Elmar.Sander@yahoo.com) VALUES ('Isabel', 'Stephan', 'Giovanni-Strobel-Gasse', '8c', '53464', 'Detmold', 'Isabel.Stephan@hotmail.com');
-INSERT INTO Kunde (Elmar, Sander, Elfi-Altmann-Straße, 8, 23340, Stendal, Elmar.Sander@yahoo.com) VALUES ('Gudrun', 'Geisler', 'Marcus-Fuhrmann-Allee', '615', '56612', 'Neu-Isenburg', 'Gudrun.Geisler@yahoo.com');
-INSERT INTO Kunde (Elmar, Sander, Elfi-Altmann-Straße, 8, 23340, Stendal, Elmar.Sander@yahoo.com) VALUES ('Wolfgang', 'Ruf', 'Melanie-Arndt-Platz', '39', '75231', 'Düsseldorf', 'Wolfgang.Ruf@hotmail.com');
-INSERT INTO Kunde (Elmar, Sander, Elfi-Altmann-Straße, 8, 23340, Stendal, Elmar.Sander@yahoo.com) VALUES ('Franz-Josef', 'Benz', 'Arnoldplatz', '6b', '13334', 'Nettetal', 'Franz-Josef.Benz@yahoo.com');
-INSERT INTO Kunde (Elmar, Sander, Elfi-Altmann-Straße, 8, 23340, Stendal, Elmar.Sander@yahoo.com) VALUES ('Sandra', 'Bertram', 'Reimannstraße', '177', '71500', 'Bad Neuenahr-Ahrweiler"', 'Sandra.Bertram@yahoo.com');
-INSERT INTO Kunde (Elmar, Sander, Elfi-Altmann-Straße, 8, 23340, Stendal, Elmar.Sander@yahoo.com) VALUES ('Karla', 'Reinhardt', 'Peer-Diehl-Gasse', '92', '84977', 'Buchholz in der Nordheide', 'Karla.Reinhardt@yahoo.com');
-INSERT INTO Kunde (Elmar, Sander, Elfi-Altmann-Straße, 8, 23340, Stendal, Elmar.Sander@yahoo.com) VALUES ('Maren', 'Dörr', 'Millerring', '81', '60034', 'Springe', 'Maren.Dörr@yahoo.com');
-INSERT INTO Kunde (Elmar, Sander, Elfi-Altmann-Straße, 8, 23340, Stendal, Elmar.Sander@yahoo.com) VALUES ('Herbert', 'Heuer', 'Helmut-Wunderlich-Straße', '921', '41513', 'Petershagen', 'Herbert.Heuer@yahoo.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Isolde', 'Baumgartner', 'Lutherstraße', '325', '24136', 'Hallstadt', 'arctic2031@outlook.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Hartwin', 'Fromm', 'Lasiuszeile', '87', '47864', 'Löningen', 'hfromm@example.org');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Yohann', 'Stein', 'Freyensteinweg', '82', '87757', 'Laage', 'stein@protonmail.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Wenzel', 'Gensch', 'Hillmannstraße', '64', '92555', 'Radolfzell', 'wenzel1@protonmail.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Imre', 'Goebel', 'Lebensbaumweg', '85', '67034', 'Hadamar', 'igoebel@yandex.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Natalia', 'Bachmeier', 'Soorstraße', '18', '04323', 'Seifhennersdorf', 'natty@yandex.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Leopold', 'Jager', 'Moorbirkenweg', '71', '53765', 'Nastätten', 'derleo@example.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Ziggy', 'Essert', 'Hinter dem Gießhaus', '240', '03152', 'Sulzbach/', 'zessert@live.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Medwin', 'Auer', 'Universitätsstraße', '39', '22661', 'Bocholt', 'm.auer@yandex.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Axel', 'Geisler', 'Hochwaldsteig', '31', '84659', 'Schotten', 'axelgeisler@outlook.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Achim', 'Beckert', 'Borkheider Straße', '132', '41732', 'Meersburg', 'beckert@duck.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Corina', 'Vogel', 'Krugpfuhlweg', '15', '94452', 'Oschersleben', 'cvogel@live.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Arndt', 'Dreschner', 'Köllnische Brücke', '19', '21112', 'Zehdenick', 'dreschnera@duck.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Anneli', 'Vogel', 'Otawistraße', '50', '47498', 'Oppenau', 'avogel@yandex.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Schwanhild', 'Beckert', 'Ella-Barowsky-Straße', '79', '77207', 'Kirchheimbolanden', 'schwanbeckert@yahoo.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Natalie', 'Bergmann', 'Erich-Thilo-Straße', '7', '86884', 'Mosbach', 'nbergmann@live.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Leopold', 'Heinrich', 'Max-Fettling-Platz', '5', '57542', 'Eilenburg', 'heinrich@live.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Neele', 'Müller', 'Im Erpelgrund', '65', '74841', 'Enger', 'neelem@protonmail.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Ernie', 'Feld', 'Im Bauernbusch', '38', '76773', 'Weil', 'erniefeld@example.org');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Kerstin', 'Adenauer', 'Waltraudstraßenbrücke', '56', '92587', 'Horn-Bad', 'kadenauer@yahoo.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Götz', 'Stein', 'Belziger Ring', '41', '37877', 'Norden', 'steingoetz@yahoo.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Katinka', 'Dohman', 'Freitaler Straße', '32', '76421', 'Schloß', 'kdohman@duck.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Dierk', 'Jung', 'Krampenburger Weg', '17', '78386', 'Höchstadt', 'djung@yahoo.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Ramona', 'Haber', 'Aarberger Straße', '80', '96680', 'Bernburg', 'rhaber@outlook.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Jana', 'Bach', 'Helgolandstraße', '87', '85315', 'Mülheim', 'linking1972@yandex.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Sabina', 'Gorman', 'Müllersteinweg', '72', '20289', 'Haselünne', 'there1927@example.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Bethold', 'Gerhard', 'Weinmeisterstraße', '7', '54772', 'Philippsburg', 'still1923@yahoo.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Theobold', 'Frank', 'Kiefheider Weg', '94', '98602', 'Borken', 'performing2008@gmail.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Lorelei', 'Schröder', 'Kolonie Müggelheim I', '96', '60007', 'Mörfelden-Walldorf', 'borders1886@example.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Hanne', 'Hutmacher', 'Drorystraße', '52', '92064', 'Neresheim', 'joan1938@outlook.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Götz', 'Brune', 'An den Feldern', '13', '04370', 'Allstedt', 'gas1815@yandex.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Clara', 'Drechsler', 'Edisonstraße', '17', '51081', 'Kirchheimbolanden', 'sustainability1917@gmail.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Calvin', 'Ludwig', 'Zangenbergweg', '15', '64490', 'Frankenberg/Sa.', 'comprehensive2010@yahoo.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Adelina', 'Hofmann', 'Suderoder Straße', '53', '57986', 'Ellrich', 'teeth1966@example.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Jacqueline', 'Kraus', 'Tiergartenufer', '5', '48116', 'Herrnhut', 'raises1965@protonmail.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Isold', 'Lehmann', 'Inge-Beisheim-Platz', '11', '07799', 'Pforzheim', 'judges1809@live.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Kristin', 'Bauers', 'Kleinaustraße', '420', '07198', 'Merzig', 'ripe1967@live.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Lotte', 'Becke', 'Dreysestraße', '69', '53345', 'Flörsheim', 'explore1860@yandex.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Axel', 'Blecher', 'Kolonie Abendruh', '72', '01457', 'Zschopau', 'alan1850@duck.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Bergen', 'Haber', 'Pepitapromenade', '14', '80736', 'Münchenbernsdorf', 'interaction1868@gmail.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Zack', 'Martin', 'Malchiner Straße', '34', '62313', 'Dahn', 'biography2053@example.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Amelia', 'Roth', 'Uwe-Liedschied-Straße', '41', '87632', 'Düsseldorf', 'colleges1844@duck.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Luther', 'Jager', 'Maipfad', '10', '64448', 'Barby', 'high2082@protonmail.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Friedrich', 'Dohman', 'Zeisigweg', '62', '50749', 'Obernburg', 'cooking2022@yandex.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Dierk', 'Brandt', 'Am Doggelhof', '97', '45807', 'Wesseling', 'blues1994@duck.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Isold', 'Alscher', 'Dyrotzer Straße', '17', '24238', 'Hückeswagen', 'circuits1877@yahoo.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Hansel', 'Andres', 'Sittestraße', '37', '03070', 'Tanna', 'tions2045@yandex.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Edwina', 'Bischoffs', 'Hansabrücke', '82', '77566', 'Friedrichsdorf', 'heavily1944@example.org');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Ellard', 'Janz', 'Friedrichsteiner Straße', '21', '69579', 'Weinheim', 'reduction2091@yandex.com');
+INSERT INTO Kunde (Vorname, Nachname, Strasse, HNr, PLZ, Stadt, Email) VALUES ('Rolly', 'Schmidt', 'Scharfensteinstraße', '72', '18755', 'Oschersleben', 'vietnamese2057@duck.com');
